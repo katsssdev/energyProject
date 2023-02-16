699e1eb937fdf6c6d4a567b102e4a169e5995a1d
katsssdev <katsiaryna.skrobuk@gmail.com>
2023-02-16 12:23:57 +0100

    change in serializer and added .gitignore

--- wind/serializers.py
+++ wind/serializers.py
@@ -5,5 +5,5 @@ from wind.models import Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('project_name', 'project_number', 'acquisition_date', 'number_3l_code', 'project_deal_type_id',
+        fields = ('project_name', 'project_number', 'number_3l_code', 'project_deal_type_id',
                   'project_group_id', 'project_status_id', 'company_id')
